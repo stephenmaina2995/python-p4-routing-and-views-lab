e514bc283472d5c3ce5631819409b717390409ad
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-06-19 12:22:01 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -4,5 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return " <h1>Python Operations with Flask Routing and Views</h1>"
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
